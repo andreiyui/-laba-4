402091757b4c57e48d756dd4a03abcd0a250a5b4
andreiyui <147510433+andreiyui@users.noreply.github.com>
2023-10-26 13:23:19 +0300

    Add files via upload

---
+++ проект4 (4).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int n;
+    int maxElement, minElement;
+    int maxIndex, minIndex;
+
+    cout << "Введите количество элементов в массиве: ";
+    cin >> n;
+
+    cout << "Введите элементы массива: ";
+    cin >> maxElement;
+    minElement = maxElement;
+    maxIndex = 0;
+    minIndex = 0;
+
+    for (int i = 1; i < n; i++) {
+        int num;
+        cin >> num;
+
+        if (num > maxElement) {
+            maxElement = num;
+            maxIndex = i;
+        }
+
+        if (num < minElement) {
+            minElement = num;
+            minIndex = i;
+        }
+    }
+
+    cout << "Максимальный элемент: " << maxElement << endl;
+    cout << "Индекс максимального элемента: " << maxIndex << endl;
+
+    cout << "Минимальный элемент: " << minElement << endl;
+    cout << "Индекс минимального элемента: " << minIndex << endl;
+
+    return 0;
+}
