c6035546dff0e41d253388546ab539ae99e46a15
andreiyui <147510433+andreiyui@users.noreply.github.com>
2023-10-26 13:10:12 +0300

    Add files via upload

---
+++ проект1 (4).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int n; 
+    cout << "Введите количество чисел в последовательности: ";
+    cin >> n;
+
+    int minNumber; 
+    int minCount = 0; 
+    
+    for (int i = 0; i < n; i++) {
+        int number;
+        cout << "Введите число: ";
+        cin >> number;
+
+        if (i == 0 || number < minNumber) {
+            minNumber = number;
+            minCount = 1;
+        } else if (number == minNumber) {
+            minCount++;
+        }
+    }
+
+    cout << "Минимальное число: " << minNumber << endl;
+    cout << "Количество повторений минимального числа: " << minCount << endl;
+
+    return 0;
+}
