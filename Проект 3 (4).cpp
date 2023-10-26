8efee4e3f72901fa76ccd01b5148e2b66cec5f31
andreiyui <147510433+andreiyui@users.noreply.github.com>
2023-10-26 14:20:24 +0300

    Add files via upload

---
+++ Проект 3 (4).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int size;
+    cout << "Введите размер массива: ";
+    cin >> size;
+
+    int count = 0;
+    int sum = 0;
+
+    cout << "Введите элементы массива:\n";
+    for (int i = 0; i < size; i++) {
+        int num;
+        cin >> num;
+
+        count++;
+
+        if (num % 3 == 0) {
+            sum += num;
+        }
+    }
+
+    cout << "Количество чисел в массиве: " << count << endl;
+    cout << "Содержимое массива: ";
+
+    for (int i = 0; i < size; i++) {
+        cout << " " << i;
+    }
+
+    cout << "\nСумма элементов, кратных 3: " << sum << endl;
+
+    return 0;
+}
