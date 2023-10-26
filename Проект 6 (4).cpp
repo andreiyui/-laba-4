198c29b184fdbf52d2a630d148388d02d4ef44ec
andreiyui <147510433+andreiyui@users.noreply.github.com>
2023-10-26 14:06:39 +0300

    Add files via upload

---
+++ Проект 6 (4).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int decimalnumber;
+    cout << "Введите десятичное число: ";
+    cin >> decimalnumber;
+
+    int binaryNumber = 0;
+    int power = 1;
+
+    while (decimalnumber > 0) {
+        int remainder = decimalnumber % 2;
+        binarynumber += remainder * power;
+        decimalNumber /= 2;
+        power *= 10;
+    }
+
+    cout << "Двоичное число: " << binarynumber << endl;
+
+    return 0;
+}
